474a7a8a58bc5eefd4979f7c0e3e1998db22fa94
JustinAMB <Justinmb@outlook.es>
2022-04-29 00:37:31 -0600

    se crea la estructura del signup

---
+++ userCases/signup/signup.js
@@ -0,0 +1,28 @@
+import { response } from '../userModule';
+
+import userRepository from '../../repositories/userRepository';
+
+
+export default signup = async(req, res = response) => {
+    try {
+        const {
+            body
+        } = req;
+        const rs = await userRepository.save(body);
+        if (rs < 1) {
+            return res.status(400).json({
+                ok: false,
+                message: 'Signup failed'
+            });
+        }
+        return res.status(201).json({
+            ok: true,
+            message: 'Signup success'
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+
+}
