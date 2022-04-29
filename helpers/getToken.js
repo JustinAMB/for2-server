c5a71b41a1a39e71286910425f6c1826ec184621
JustinAMB <Justinmb@outlook.es>
2022-04-29 00:46:20 -0600

    se cambia el parametro id por email

--- helpers/getToken.js
+++ helpers/getToken.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
-export default getToken = (id) => {
-    const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+export default getToken = (email) => {
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
         expiresIn: '24h'
     });
     return token;
