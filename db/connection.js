8e933e30d7b2fe71855b4736816f7da08ab8559e
JustinAMB <Justinmb@outlook.es>
2022-04-28 18:07:11 -0600

    se crea la conexion con la base de datos

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+import mysql, { Pool } from 'mysql2/promise';
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    insecureAuth: true,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+export default pool;
