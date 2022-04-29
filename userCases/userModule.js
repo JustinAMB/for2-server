c60594a4ea4ac2472bb878e8179bb05f3a603ece
JustinAMB <Justinmb@outlook.es>
2022-04-29 00:25:27 -0600

    se crea el modulo de los casos para la importaciones en comun

---
+++ userCases/userModule.js
@@ -0,0 +1,10 @@
+import { response } from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { v4 } from 'uuid';
+export {
+    response,
+    bcrypt,
+    jwt,
+    v4
+}
