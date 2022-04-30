510ee231062e2c70d602916155768da1447013a0
JustinAMB <Justinmb@outlook.es>
2022-04-29 23:46:27 -0600

    se agrega la encriptacion del password

--- userCases/signup/signup.js
+++ userCases/signup/signup.js
@@ -2,12 +2,14 @@ import { response } from '../userModule';
 
 import userRepository from '../../repositories/userRepository';
 import getToken from '../../helpers/getToken';
-
+import { encryptPassword } from '../../helpers/encryptPassword';
 export default signup = async(req, res = response) => {
     try {
         const {
             body
         } = req;
+        body.image = 'no-image.jpg';
+        body.password = await encryptPassword(body.password);
         body.token = getToken(body.email);
         const rs = await userRepository.save(body);
         if (rs < 1) {
